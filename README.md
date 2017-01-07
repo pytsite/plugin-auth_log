44ce2c8d5df6cc10d4c63e1df89e6665aa77f1f3
Alexander Shepetko <a@shepetko.com>
2017-01-08 00:57:14 +0200

    Release 0.1.2

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Authentication events logging.
 
 ## Changelog
 
+### 0.1.2 (2017-01-08)
+Translations updated.
+
 ### 0.1.1 (2016-12-19)
 plugin.json updated.
-
 
 ### 0.1 (2016-11-28)
 First release.
--- __init__.py
+++ __init__.py
@@ -8,7 +8,6 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 # Resources
-_lang.register_package(__name__)  # odm_auth requires this
 _lang.register_package(__name__, alias='auth_log')
 
 # ODM models
@@ -22,4 +21,4 @@ _events.listen('pytsite.auth.sign_in_error', _eh.auth_sign_in_error)
 # Admin sidebar menu item
 admin_href = _router.ep_path('pytsite.odm_ui@browse', {'model': 'auth_log'})
 _admin.sidebar.add_menu('auth', 'auth_log', 'auth_log@log', admin_href, 'fa fa-history', weight=30,
-                        permissions='pytsite.odm_perm.delete.auth_log')
+                        permissions='pytsite.odm_auth.delete.auth_log')
